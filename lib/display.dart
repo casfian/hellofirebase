111f16e1334dfb6333900238098398291440663f
BB Caspian <casfian@gmail.com>
2021-08-24 12:16:01 +0800

    added streambuilder

--- lib/display.dart
+++ lib/display.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Display extends StatelessWidget {
   @override
@@ -7,11 +8,31 @@ class Display extends StatelessWidget {
       appBar: AppBar(
         title: Text('Display Data'),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Hello World'),
-        ),
-      ),
+      body: StreamBuilder<QuerySnapshot>(
+          initialData: null, //data awal kita
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .snapshots(), //sumber data kita
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Center(child: Text('Error'));
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data() as Map<String, dynamic>;
+                return ListTile(
+                  title: Text(data['name']),
+                  subtitle: Text(data['email']),
+                  trailing: Text(data['age']),
+                );
+              }).toList(),
+            );
+          } //paparkan data kita
+          ),
     );
   }
 }
